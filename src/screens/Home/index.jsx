9d1c6f18e28db15c28bd1775e77c707144a5e977
matheusgondra <matheusgondra951@gmail.com>
2022-08-22 21:28:37 -0300

    Criando pagina de home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,9 +1,16 @@
 import { Header } from "../../components/Header";
+import { Content } from "./styles";
 
 export function Home() {
 	return (
 		<>
 			<Header />
+			<Content>
+				<h1>Bem vindo <br />ao <br />Study Notes</h1>
+
+				<button type="button">Fazer login</button>
+				<button type="button">Criar conta</button>
+			</Content>
 		</>
 	);
 }
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Content = styled.main`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: 114px;
+	margin-left: 40px;
+	margin-right: 40px;
+
+	h1 {
+		font-weight: 700;
+		font-size: 30px;
+		color: white;
+		margin-bottom: 88px;
+		text-align: center;
+	}
+
+	button {
+		width: 180px;
+		height: 42px;
+		color: white;
+		font-size: 18px;
+		background-color: #1953C7;
+		border-radius: 8px;
+		outline: none;
+		border: none;
+		margin-bottom: 22px;
+	}
+`;
